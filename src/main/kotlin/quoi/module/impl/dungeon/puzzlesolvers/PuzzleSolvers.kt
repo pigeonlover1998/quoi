ff535b2ff851b9d9dbe603e4f242628da36b26a5
Kylen <200985205+Kylen-V@users.noreply.github.com>
2026-03-11 19:45:35 +0800

    fix: Auto Puzzles depending on solvers being enabled x2 phrog still loves me

--- src/main/kotlin/quoi/module/impl/dungeon/puzzlesolvers/PuzzleSolvers.kt
+++ src/main/kotlin/quoi/module/impl/dungeon/puzzlesolvers/PuzzleSolvers.kt
@@ -20,29 +20,29 @@ object PuzzleSolvers : Module(
     area = Island.Dungeon(inClear = true)
 ) {
     private val fillDropdown by DropdownSetting("Ice fill").collapsible()
-    private val fillSolver by BooleanSetting("Toggle", desc = "Shows the solution for the ice fill puzzle.").json("Ice fill solver toggle").withDependency(fillDropdown)
+    private val fillSolver by BooleanSetting("Solver", desc = "Shows the solution for the ice fill puzzle.").json("Ice fill solver toggle").withDependency(fillDropdown)
     private val fillColour by ColourSetting("Colour", Colour.MAGENTA, allowAlpha = true).json("Ice fill colour").withDependency(fillDropdown) { fillSolver }
-    private val fillAuto by BooleanSetting("Auto", desc = "Automatically completes the ice fill puzzle.").json("Auto ice fill").withDependency(fillDropdown) { fillSolver }
-    private val fillDelay by NumberSetting("Delay", 2, 1, 10, 1, unit = "t").json("Auto ice fill delay").withDependency(fillDropdown) { fillSolver && fillAuto }
+    private val fillAuto by BooleanSetting("Auto", desc = "Automatically completes the ice fill puzzle.").json("Auto ice fill").withDependency(fillDropdown)
+    private val fillDelay by NumberSetting("Delay", 2, 1, 10, 1, unit = "t").json("Auto ice fill delay").withDependency(fillDropdown) { fillAuto }
 
     private val mazeDropdown by DropdownSetting("Teleport maze").collapsible()
-    private val mazeSolver by BooleanSetting("Toggle", desc = "Shows the solution for the TP maze puzzle.").json("Maze solver toggle").withDependency(mazeDropdown)
+    private val mazeSolver by BooleanSetting("Solver", desc = "Shows the solution for the TP maze puzzle.").json("Maze solver toggle").withDependency(mazeDropdown)
     private val mazeColourOne by ColourSetting("Colour for one", Colour.MINECRAFT_GREEN.withAlpha(0.5f), true, desc = "Colour for when there is a single solution.").withDependency(mazeDropdown) { mazeSolver }
     private val mazeColourMultiple by ColourSetting("Colour for multiple", Colour.MINECRAFT_GOLD.withAlpha(0.5f), true, desc = "Colour for when there are multiple solutions.").withDependency(mazeDropdown) { mazeSolver }
     private val mazeColourVisited by ColourSetting("Colour for visited", Colour.MINECRAFT_RED.withAlpha(0.5f), true, desc = "Colour for the already used TP pads.").withDependency(mazeDropdown) { mazeSolver }
-    private val mazeAuto by BooleanSetting("Auto").json("Auto maze").withDependency(mazeDropdown) { mazeSolver }
+    private val mazeAuto by BooleanSetting("Auto").json("Auto maze").withDependency(mazeDropdown)
 
     private val beamsDropdown by DropdownSetting("Creeper beams").collapsible()
-    private val beamsSolver by BooleanSetting("Toggle", desc = "Shows the solution for the creeper beams puzzle.").json("Creeper beams solver toggle").withDependency(beamsDropdown)
-    private val beamsAnnounce by BooleanSetting("Announce completion").withDependency(beamsDropdown) { beamsSolver }
+    private val beamsSolver by BooleanSetting("Solver", desc = "Shows the solution for the creeper beams puzzle.").json("Creeper beams solver toggle").withDependency(beamsDropdown)
+    private val beamsAnnounce by BooleanSetting("Announce completion").withDependency(beamsDropdown)
     private val beamsTracer by BooleanSetting("Tracer").json("Beams tracer").withDependency(beamsDropdown) { beamsSolver }
     private val beamsStyle by SelectorSetting("Style", "Box", arrayListOf("Box", "Filled box"), desc = "Render style to be used.").json("Beams style").withDependency(beamsDropdown) { beamsSolver }
     private val beamsAlpha by NumberSetting("Colour alpha", 0.7f, 0f, 1f, 0.05f).json("Beams colour alpha").withDependency(beamsDropdown) { beamsSolver }
-    private val beamsAuto by BooleanSetting("Auto").json("Auto beams").withDependency(beamsDropdown) { beamsSolver }
+    private val beamsAuto by BooleanSetting("Auto").json("Auto beams").withDependency(beamsDropdown)
 
     private val blazeDropdown by DropdownSetting("Blaze").collapsible()
-    private val blazeSolver by BooleanSetting("Toggle", desc = "Shows the solution for the blaze puzzle.").json("Blaze solver").withDependency(blazeDropdown)
-    private val blazeAnnounce by BooleanSetting("Announce completion", desc = "Send complete message.").withDependency(blazeDropdown) { blazeSolver }
+    private val blazeSolver by BooleanSetting("Solver", desc = "Shows the solution for the blaze puzzle.").json("Blaze solver").withDependency(blazeDropdown)
+    private val blazeAnnounce by BooleanSetting("Announce completion", desc = "Send complete message.").withDependency(blazeDropdown)
     private val blazeLineNext by BooleanSetting("Next line", desc = "Shows the next line to click.").json("Blaze solver next line").withDependency(blazeDropdown) { blazeSolver }
     private val blazeLineAmount by NumberSetting("Lines amount", 1, 1, 10, 1, desc = "Amount of lines to show.").json("Blaze solver lines amount").withDependency(blazeDropdown) { blazeSolver && blazeLineNext }
     private val blazeLineWidth by NumberSetting("Lines width", 2f, 0.5f, 5f, 0.1f, desc = "Width for blaze lines.").json("Blaze solver lines width").withDependency(blazeDropdown) { blazeSolver && blazeLineNext }
@@ -50,17 +50,17 @@ object PuzzleSolvers : Module(
     private val blazeFirstColour by ColourSetting("First colour", Colour.MINECRAFT_GREEN.withAlpha(.75f), desc = "Colour for the first blaze.").withDependency(blazeDropdown) { blazeSolver }
     private val blazeSecondColour by ColourSetting("Second colour", Colour.MINECRAFT_GOLD.withAlpha(.75f), desc = "Colour for the second blaze.").withDependency(blazeDropdown) { blazeSolver }
     private val blazeAllColour by ColourSetting("Other colour", Colour.WHITE.withAlpha(.3f), desc = "Colour for the other blazes.").withDependency(blazeDropdown) { blazeSolver }
-    private val blazeAuto by BooleanSetting("Auto").json("Auto blaze").withDependency(blazeDropdown) { blazeSolver }
-    private val blazeReposition by BooleanSetting("Auto reposition").json("Blaze auto reposition").withDependency(blazeDropdown) { blazeSolver && blazeAuto }
+    private val blazeAuto by BooleanSetting("Auto").json("Auto blaze").withDependency(blazeDropdown)
+    private val blazeReposition by BooleanSetting("Auto reposition").json("Blaze auto reposition").withDependency(blazeDropdown) { blazeAuto }
 
-    private val bowDropdown by DropdownSetting("Bow settings").collapsible().withDependency { beamsSolver && beamsAuto || blazeSolver && blazeAuto}
+    private val bowDropdown by DropdownSetting("Bow settings").collapsible().withDependency { beamsAuto || blazeAuto}
     private val shootCd by NumberSetting("Shoot cooldown", 500L, 250L, 1000L, 50L, unit = "ms").withDependency(bowDropdown)
     private val missCd by NumberSetting("Miss cooldown", 550L, 300L, 1050L, 50L, unit = "ms").withDependency(bowDropdown)
 
     init {
         scheduleLoop(10) {
             if (!enabled || currentRoom?.data?.type != RoomType.PUZZLE) return@scheduleLoop
-            if (blazeSolver) BlazeSolver.getBlaze()
+            if (blazeSolver || blazeAuto) BlazeSolver.getBlaze()
         }
 
         on<WorldEvent.Change> {
@@ -78,29 +78,29 @@ object PuzzleSolvers : Module(
         }
 
         on<RenderEvent.World> {
-            if (fillSolver)  IceFillSolver.onRenderWorld(ctx, fillColour)
+            if (fillSolver) IceFillSolver.onRenderWorld(ctx, fillColour)
             if (beamsSolver) BeamsSolver.onRenderWorld(ctx, beamsStyle.selected, beamsTracer, beamsAlpha)
             if (blazeSolver) BlazeSolver.onRenderWorld(ctx, blazeLineNext, blazeLineAmount, blazeStyle.selected, blazeFirstColour, blazeSecondColour, blazeAllColour, blazeAnnounce, blazeLineWidth, blazeReposition)
-            if (mazeSolver)  MazeSolver.onRenderWorld(ctx, mazeColourOne, mazeColourMultiple, mazeColourVisited)
+            if (mazeSolver) MazeSolver.onRenderWorld(ctx, mazeColourOne, mazeColourMultiple, mazeColourVisited)
         }
 
         on<TickEvent.End> {
             if (currentRoom?.data?.type != RoomType.PUZZLE) return@on
-            if (fillSolver && fillAuto)   IceFillSolver.onTick(player, fillDelay)
-            if (beamsSolver && beamsAuto) BeamsSolver.onTick(player, shootCd, missCd)
-            if (blazeSolver && blazeAuto) BlazeSolver.onTick(player, shootCd, missCd, blazeReposition)
-            if (mazeSolver && mazeAuto)   MazeSolver.onTick(player)
+            if (fillAuto) IceFillSolver.onTick(player, fillDelay)
+            if (beamsAuto) BeamsSolver.onTick(player, shootCd, missCd)
+            if (blazeAuto) BlazeSolver.onTick(player, shootCd, missCd, blazeReposition)
+            if (mazeAuto) MazeSolver.onTick(player)
         }
 
         on<BlockUpdateEvent> {
-            if (beamsSolver) BeamsSolver.onBlockChange(this@on, beamsAnnounce)
+            if (beamsSolver || beamsAuto) BeamsSolver.onBlockChange(this@on, beamsAnnounce)
         }
 
         on<PacketEvent.Received> {
             if (currentRoom?.data?.type != RoomType.PUZZLE) return@on
             when (packet) {
-                is ClientboundSoundPacket ->          if (beamsSolver && beamsAuto) BeamsSolver.onSound(packet)
-                is ClientboundPlayerPositionPacket -> if (mazeSolver) MazeSolver.onPosition(packet)
+                is ClientboundSoundPacket -> if (beamsSolver || beamsAuto) BeamsSolver.onSound(packet)
+                is ClientboundPlayerPositionPacket -> if (mazeSolver || mazeAuto) MazeSolver.onPosition(packet)
             }
         }
     }
