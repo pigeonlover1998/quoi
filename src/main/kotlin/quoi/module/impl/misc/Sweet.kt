ef782f0bed2053207308ec64b0d040cb5b2e91f3
Kylen <200985205+Kylen-V@users.noreply.github.com>
2026-03-20 17:10:41 +0800

    super sweet

--- src/main/kotlin/quoi/module/impl/misc/Sweet.kt
+++ src/main/kotlin/quoi/module/impl/misc/Sweet.kt
@@ -7,7 +7,7 @@ import quoi.utils.Sounds
 import quoi.utils.StringUtils.noControlCodes
 
 object Sweet : Module("Sweet") {
-    private val soundVolume by slider("Volume", 1.0f, 0.1f, 2.0f, 0.01f, "Volume of the sound.")
+    private val soundVolume by slider("Volume", 1.0f, 0.1f, 10.0f, 0.01f, "Volume of the sound.")
     private val soundPitch by slider("Pitch", 1.0f, 0.1f, 2.0f, 0.01f, "Pitch of the sound.")
 
     init {
