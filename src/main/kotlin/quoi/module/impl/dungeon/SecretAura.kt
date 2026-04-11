1cf580ea3014921d6575dcd2a34c61c4eca1c644
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-04-12 02:57:30 +0300

    feat: merge lever aura into secret aura

--- src/main/kotlin/quoi/module/impl/dungeon/SecretAura.kt
+++ src/main/kotlin/quoi/module/impl/dungeon/SecretAura.kt
@@ -1,5 +1,7 @@
 package quoi.module.impl.dungeon
 
+import it.unimi.dsi.fastutil.longs.Long2LongOpenHashMap
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet
 import net.minecraft.core.BlockPos
 import net.minecraft.core.component.DataComponents
 import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket
@@ -10,6 +12,7 @@ import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.decoration.ArmorStand
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.LeverBlock
 import net.minecraft.world.level.block.SkullBlock
 import net.minecraft.world.level.block.entity.ChestBlockEntity
 import net.minecraft.world.level.block.entity.SkullBlockEntity
@@ -19,15 +22,13 @@ import quoi.api.events.PacketEvent
 import quoi.api.events.TickEvent
 import quoi.api.events.WorldEvent
 import quoi.api.skyblock.Location.inSkyblock
+import quoi.api.skyblock.dungeon.Dungeon
 import quoi.api.skyblock.dungeon.Dungeon.currentRoom
-import quoi.api.skyblock.dungeon.Dungeon.inBoss
 import quoi.api.skyblock.dungeon.Dungeon.inDungeons
 import quoi.module.Module
 import quoi.module.settings.UIComponent.Companion.childOf
-import quoi.utils.Vec3
+import quoi.utils.*
 import quoi.utils.WorldUtils.state
-import quoi.utils.aabb
-import quoi.utils.equalsOneOf
 import quoi.utils.skyblock.player.AuraManager
 import quoi.utils.skyblock.player.SwapManager
 import quoi.utils.skyblock.player.SwapResult
@@ -48,18 +49,41 @@ object SecretAura : Module(
 
     private val swing by switch("Swing hand", desc = "Makes secret aura swing hand on click.")
     private val dungeonsOnly by switch("Dungeons only", true, desc = "Makes secret aura only work in dungeons.")
+    private val inBoss by switch("In boss", true, desc = "Makes secret aura work in floor 7 boss.")
+    private val inContainer by switch("In container", desc = "Makes secret aura work while container is opened.")
 
     private val REDSTONE_KEY = UUID.fromString("fed95410-aba1-39df-9b95-1d4f361eb66e")
     private val WITHER_ESSENCE = UUID.fromString("e0f3e929-869e-3dca-9504-54c666ee6f23")
 
     private var redstoneKey = true
-    private val clickedBlocks = mutableMapOf<BlockPos, Long>()
-    val blocksDone = mutableSetOf<BlockPos>()
-    var previousSlot = -1
+    private val clickedBlocks = Long2LongOpenHashMap()
+    val blocksDone = LongOpenHashSet()
+    private var previousSlot = -1
     var lastClickedPos: BlockPos? = null
 
+    private val levers = listOf(
+        BlockPos(94, 124, 113),
+        BlockPos(106, 124, 113),
+        BlockPos(27, 124, 127),
+        BlockPos(23, 132, 138),
+        BlockPos(14, 122, 55),
+        BlockPos(2, 122, 55),
+        BlockPos(86, 128, 46),
+        BlockPos(84, 121, 34)
+    )
 
-    private data class BlockDistance(val block: Block, val pos: BlockPos, val distanceSq: Double)
+    private val deviceLevers = listOf(
+        BlockPos(62, 136, 142),
+        BlockPos(62, 133, 142),
+
+        BlockPos(60, 134, 142),
+        BlockPos(60, 135, 142),
+
+        BlockPos(58, 133, 142),
+        BlockPos(58, 136, 142)
+    )
+
+    private val extraDevLever = BlockPos(59, 133, 142)
 
     override fun onDisable() {
         super.onDisable()
@@ -76,21 +100,28 @@ object SecretAura : Module(
         }
 
         on<TickEvent.End> {
-            if (mc.screen != null || !inSkyblock || inBoss || (!inDungeons && dungeonsOnly)) return@on
+            if (!inSkyblock ||
+                (mc.screen != null && !inContainer) ||
+                (dungeonsOnly && !inDungeons) ||
+                (Dungeon.inBoss && !inBoss)
+            ) return@on
 
-            clickedBlocks.entries.removeIf { (pos, time) ->
+            val currentTime = System.currentTimeMillis()
+            val iterator = clickedBlocks.long2LongEntrySet().fastIterator()
 
-                if (blocksDone.contains(pos) || System.currentTimeMillis() - time > 2000) {
-                    return@removeIf true
+            while (iterator.hasNext()) {
+                val entry = iterator.next()
+                val posLong = entry.longKey
+                if (blocksDone.contains(posLong) || currentTime - entry.longValue > 2000) {
+                    iterator.remove()
+                    continue
                 }
 
-                val tile = level.getBlockEntity(pos)
+                val tile = level.getBlockEntity(BlockPos.of(posLong))
                 if (tile is ChestBlockEntity && tile.getOpenNess(0f) > 0f) {
-                    blocksDone.add(pos)
-                    return@removeIf true
+                    blocksDone.add(posLong)
+                    iterator.remove()
                 }
-
-                false
             }
 
             currentRoom?.let { room ->
@@ -108,7 +139,7 @@ object SecretAura : Module(
             }
 
             var blockCandidate = BlockDistance(Blocks.AIR, BlockPos(Int.MAX_VALUE, 69, Int.MIN_VALUE), Double.POSITIVE_INFINITY)
-            val eyePos = player.eyePosition
+            val eyePos = player.eyePosition()
 
             val sqEssence = skullRange * skullRange
             val sqChest = chestRange * chestRange
@@ -119,25 +150,30 @@ object SecretAura : Module(
             val maxPos = BlockPos.containing(searchBox.maxX, searchBox.maxY, searchBox.maxZ)
 
             for (pos in BlockPos.betweenClosed(minPos, maxPos)) {
-                if (blocksDone.contains(pos)) continue
+                val posLong = pos.asLong()
+                if (blocksDone.contains(posLong)) continue
 
-                val currentBlock = level.getBlockState(pos).block
-                if (!isValidBlock(currentBlock, pos)) continue
+                val state = pos.state
+                val block = state.block
 
-                val nextClickTime = clickedBlocks[pos]
-                if (clickDelay > 0 && nextClickTime == null) {
-                    clickedBlocks[pos.immutable()] = System.currentTimeMillis() + clickDelay
+                if (Dungeon.inBoss && !pos.isBossBlock(state)) continue
+
+                if (!isValidBlock(block, pos)) continue
+
+                val nextClickTime = clickedBlocks[posLong]
+                if (clickDelay > 0 && nextClickTime == 0L) {
+                    clickedBlocks[pos.immutable().asLong()] = currentTime + clickDelay
                     continue
                 }
 
-                if (nextClickTime != null && nextClickTime > System.currentTimeMillis()) continue
+                if (nextClickTime != 0L && nextClickTime > currentTime) continue
 
                 val currentDistanceSq = eyePos.distanceToSqr(pos.center)
 
-                if ((currentBlock is SkullBlock && currentDistanceSq > sqEssence) || currentDistanceSq > sqChest) continue
+                if ((block is SkullBlock && currentDistanceSq > sqEssence) || currentDistanceSq > sqChest) continue
 
                 if (currentDistanceSq < blockCandidate.distanceSq) {
-                    blockCandidate = BlockDistance(currentBlock, pos.immutable(), currentDistanceSq)
+                    blockCandidate = BlockDistance(block, pos.immutable(), currentDistanceSq)
                 }
             }
             currentRoom?.let { room ->
@@ -173,7 +209,7 @@ object SecretAura : Module(
                 }
             }
 
-            clickedBlocks[blockCandidate.pos] = System.currentTimeMillis() + 500
+            clickedBlocks[blockCandidate.pos.asLong()] = System.currentTimeMillis() + 500
 
             lastClickedPos = blockCandidate.pos
             AuraManager.interactBlock(blockCandidate.pos)
@@ -205,7 +241,7 @@ object SecretAura : Module(
                             val profile = itemStack.get(DataComponents.PROFILE)
 
                             if (profile?.partialProfile()?.id == WITHER_ESSENCE) {
-                                blocksDone.add(entity.blockPosition().offset(0, 2, 0).immutable())
+                                blocksDone.add(entity.blockPosition().offset(0, 2, 0).immutable().asLong())
                             }
                         }
                     }
@@ -237,7 +273,10 @@ object SecretAura : Module(
         val currentBlock = state.block
 
         if (currentBlock === Blocks.LEVER) {
-            blocksDone.add(pos)
+            val bossLev = pos in levers || pos in deviceLevers || pos == extraDevLever
+            if (!bossLev) {
+                blocksDone.add(pos.asLong())
+            }
             return
         }
 
@@ -250,8 +289,16 @@ object SecretAura : Module(
         }
 
         if (currentBlock === Blocks.REDSTONE_BLOCK) {
-            blocksDone.add(pos)
+            blocksDone.add(pos.asLong())
         }
+    }
+
+    private fun BlockPos.isBossBlock(state: BlockState): Boolean {
+        if (state.block != Blocks.LEVER) return false
+        val p3Lever = this in levers && EntityUtils.getEntities<ArmorStand>(vec3.aabb(0.5)) { it.displayName?.string == "Not Activated" }.isNotEmpty()
+        val devLever = this in deviceLevers && state.hasProperty(LeverBlock.POWERED) && !state.getValue(LeverBlock.POWERED)
+        val extraDevLever = this == extraDevLever && !devLever && EntityUtils.getEntities<ArmorStand>(vec3.aabb(2.0)) { it.displayName?.string == "Inactive" }.isNotEmpty() // untested
+        return p3Lever || devLever || extraDevLever
     }
 
     fun clear() {
@@ -260,4 +307,6 @@ object SecretAura : Module(
         redstoneKey = false
         previousSlot = -1
     }
+
+    private data class BlockDistance(val block: Block, val pos: BlockPos, val distanceSq: Double)
 }
