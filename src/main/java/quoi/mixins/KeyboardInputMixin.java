219c07e8e10e0d1be90c75cabc2202324662c668
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-04-21 14:14:32 +0300

    fix: rsm compat

--- src/main/java/quoi/mixins/KeyboardInputMixin.java
+++ src/main/java/quoi/mixins/KeyboardInputMixin.java
@@ -1,17 +1,18 @@
 package quoi.mixins;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.player.KeyboardInput;
 import net.minecraft.world.entity.player.Input;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import quoi.api.events.KeyEvent;
 import quoi.api.input.MutableInput;
 
 @Mixin(KeyboardInput.class)
 public class KeyboardInputMixin {
-    @Redirect(
+    @WrapOperation(
             method = "tick",
             at = @At(
                     value = "FIELD",
@@ -19,7 +20,7 @@ public class KeyboardInputMixin {
                     opcode = Opcodes.PUTFIELD
             )
     )
-    private void onTick(KeyboardInput instance, Input input) {
+    private void onTick(KeyboardInput instance, Input input, Operation<Void> original) {
         KeyEvent.Input event = new KeyEvent.Input(input, new MutableInput(input));
         event.post();
         instance.keyPresses = event.getInput().toInput();
