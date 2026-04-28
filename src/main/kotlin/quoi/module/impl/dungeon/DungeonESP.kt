efa4244d517a92780377a70c0aaf86f06085d7af
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-04-28 21:33:08 +0300

    fix(dungeonesp): start esp highlighting all fels

--- src/main/kotlin/quoi/module/impl/dungeon/DungeonESP.kt
+++ src/main/kotlin/quoi/module/impl/dungeon/DungeonESP.kt
@@ -21,8 +21,8 @@ import quoi.module.Module
 import quoi.module.settings.Setting.Companion.json
 import quoi.module.settings.UIComponent.Companion.childOf
 import quoi.module.settings.UIComponent.Companion.visibleIf
-import quoi.utils.EntityUtils
 import quoi.utils.EntityUtils.getEntities
+import quoi.utils.EntityUtils.getEntity
 import quoi.utils.EntityUtils.interpolatedBox
 import quoi.utils.Scheduler.scheduleLoop
 import quoi.utils.StringUtils.noControlCodes
@@ -33,7 +33,7 @@ object DungeonESP : Module(
     "Dungeon ESP",
     desc = "Highlights various dungeon entities.",
     area = Island.Dungeon(inClear = true)
-) { // todo recode star esp
+) {
     private val teammateClassGlow by switch("Teammate class glow", true, desc = "Highlights dungeon teammates based on their class colour.")
     private val starEsp by switch("Starred mobs")
 
@@ -133,8 +133,15 @@ object DungeonESP : Module(
 
     private fun getColour(entity: Entity) = when (entity) {
         is Bat if (entity.maxHealth.equalsOneOf(100f, 200f, 400f, 800f)) -> colourBat to colourBatFill
+//        is EnderMan if (entity.name.string == "Dinnerbone") -> {
+//            colourStar to colourStarFill
+//        }
         is EnderMan if (entity.name.string == "Dinnerbone") -> {
-            colourStar to colourStarFill
+            val stand = getEntity(entity.id + 1) as? ArmorStand
+
+            if (stand?.customName?.string?.contains("✯") == true) {
+                colourStar to colourStarFill
+            } else null
         }
         is ArmorStand -> {
             handleStand(entity)
