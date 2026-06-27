501500506ce5ccd679010fe4a5be0817462401b8
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-06-27 18:39:34 +0300

    feat(tweaks): no cursor reset original: https://github.com/odtheking/Odin/blob/main/src/main/kotlin/com/odtheking/odin/features/impl/skyblock/NoCursorReset.kt

--- src/main/java/quoi/mixins/MouseHandlerMixin.java
+++ src/main/java/quoi/mixins/MouseHandlerMixin.java
@@ -1,5 +1,9 @@
 package quoi.mixins;
 
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.gui.screens.inventory.ContainerScreen;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Shadow;
 import quoi.api.events.MouseEvent;
 import quoi.module.impl.misc.chat.impl.ChatPeek;
 import net.minecraft.client.Minecraft;
@@ -10,9 +14,20 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import quoi.module.impl.player.Tweaks;
 
 @Mixin(MouseHandler.class)
 public class MouseHandlerMixin {
+
+    @Shadow
+    private double xpos;
+    @Shadow
+    private double ypos;
+
+    @Unique
+    private double beforeX;
+    @Unique
+    private double beforeY;
 
     @Inject(
             method = "onScroll",
@@ -54,5 +69,33 @@ public class MouseHandlerMixin {
     private boolean checkShit(long window) {
         Minecraft mc = Minecraft.getInstance();
         return window != mc.getWindow().handle() || mc.screen != null;
+    }
+
+    /**
+     * from OdinFabric (BSD 3-Clause)
+     * copyright (c) 2025-2026 odtheking
+     * original: https://github.com/odtheking/Odin/blob/main/src/main/java/com/odtheking/mixin/mixins/MouseHandlerMixin.java
+     */
+    @Inject(
+            method = "grabMouse",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/MouseHandler;xpos:D",
+                    ordinal = 0,
+                    opcode = Opcodes.PUTFIELD
+            )
+    )
+    private void odin$lockXPos(CallbackInfo ci) {
+        this.beforeX = this.xpos;
+        this.beforeY = this.ypos;
+    }
+
+    @Inject(method = "releaseMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;getWindow()Lcom/mojang/blaze3d/platform/Window;"))
+    private void odin$correctCursorPosition(CallbackInfo ci) {
+        if (Minecraft.getInstance().screen instanceof ContainerScreen && Tweaks.shouldHookMouse()) {
+            InputConstants.grabOrReleaseMouse(Minecraft.getInstance().getWindow(), InputConstants.CURSOR_NORMAL, this.beforeX, this.beforeY);
+            this.xpos = this.beforeX;
+            this.ypos = this.beforeY;
+        }
     }
 }
--- src/main/kotlin/quoi/module/impl/player/Tweaks.kt
+++ src/main/kotlin/quoi/module/impl/player/Tweaks.kt
@@ -1,5 +1,7 @@
 package quoi.module.impl.player
 
+import quoi.api.events.TickEvent
+import quoi.api.events.core.on
 import quoi.api.skyblock.Location.inSkyblock
 import quoi.api.skyblock.Location.onModernIsland
 import quoi.module.Module
@@ -16,9 +18,36 @@ object Tweaks : Module(
     @JvmStatic val disableItemCooldowns by switch("Disable item cooldowns", desc = "Disables item cooldowns such as ender pearls.").childOf(::skyblockOnly)
     @JvmStatic val fixInteract by switch("Fix interaction", desc = "Fixes a bug where you can't interact when SA jumps the player.").childOf(::skyblockOnly)
 
+    /**
+     * from OdinFabric (BSD 3-Clause)
+     * copyright (c) 2025-2026 odtheking
+     * original: https://github.com/odtheking/Odin/blob/main/src/main/kotlin/com/odtheking/odin/features/impl/skyblock/NoCursorReset.kt
+     */
+    private val noCursorReset by switch("No cursor reset").childOf(::skyblockOnly)
+
+    private var time = System.currentTimeMillis()
+    private var wasNotNull = false
+
+    init {
+        on<TickEvent.End> {
+            if (mc.screen != null) {
+                wasNotNull = true
+                time = System.currentTimeMillis()
+            } else if (wasNotNull && mc.screen == null) {
+                wasNotNull = false
+                time = System.currentTimeMillis()
+
+            }
+        }
+    }
+
     @JvmStatic
     fun should(condition: Boolean): Boolean = this.enabled && condition // idkman
 
     @JvmStatic
-    fun shouldSb(condition: Boolean): Boolean = this.enabled && inSkyblock && !onModernIsland && condition
+    fun shouldSb(condition: Boolean): Boolean = this.enabled && inSkyblock && condition
+
+    @JvmStatic
+    fun shouldHookMouse(): Boolean =
+        System.currentTimeMillis() - time < 150 && shouldSb(noCursorReset)
 }
