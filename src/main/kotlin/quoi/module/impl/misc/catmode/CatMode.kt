05d683242a041f7571771c41644be7495946d37b
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-07-04 20:43:23 +0300

    feat(catmode): keep formatting in meowText and fix meowSond crash (maybe)

--- src/main/kotlin/quoi/module/impl/misc/catmode/CatMode.kt
+++ src/main/kotlin/quoi/module/impl/misc/catmode/CatMode.kt
@@ -1,6 +1,8 @@
 package quoi.module.impl.misc.catmode
 
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.Component.literal
+import net.minecraft.network.chat.Style
 import net.minecraft.network.protocol.game.ClientboundSoundPacket
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.util.FormattedCharSequence
@@ -8,6 +10,7 @@ import quoi.api.events.PacketEvent
 import quoi.api.events.core.on
 import quoi.module.Module
 import quoi.module.impl.misc.catmode.impl.*
+import quoi.utils.StringUtils.FORMATTING_CODE_PATTERN
 
 /**
  * https://github.com/Noamm9/CatgirlAddons/blob/main/src/main/kotlin/catgirlroutes/module/impl/misc/CatMode.kt
@@ -31,16 +34,18 @@ object CatMode : Module(
             if (!meowSound || packet.sound == SoundEvents.CAT_AMBIENT_BABY) return@on
 
             cancel()
-            level.playLocalSound(
-                packet.x,
-                packet.y,
-                packet.z,
-                SoundEvents.CAT_AMBIENT_BABY.value(),
-                packet.source,
-                packet.volume,
-                packet.pitch,
-                false
-            )
+            mc.execute {
+                level.playLocalSound(
+                    packet.x,
+                    packet.y,
+                    packet.z,
+                    SoundEvents.CAT_AMBIENT_BABY.value(),
+                    packet.source,
+                    packet.volume,
+                    packet.pitch,
+                    false
+                )
+            }
         }
     }
 
@@ -54,20 +59,39 @@ object CatMode : Module(
     fun replaceText(text: FormattedCharSequence): FormattedCharSequence {
         if (!enabled || !meowText) return text
 
-        val original = buildString {
-            text.accept { _, _, codePoint ->
-                appendCodePoint(codePoint)
-                true
+        val result = Component.empty()
+        var style: Style? = null
+
+        text.accept { _, s, codepoint ->
+            val c = codepoint.toChar()
+            if (c.isLetter()) {
+                style = style ?: s
+            } else {
+                style?.let { result.append(literal("meow").withStyle(it)) }
+                style = null
+
+                result.append(literal("$c").withStyle(s))
             }
+            true
         }
-        val replaced = meowify(original)
-        return if (replaced == original) text else Component.literal(replaced).visualOrderText
+
+        style?.let { result.append(literal("meow").withStyle(it)) }
+
+        return result.visualOrderText
     }
 
     private fun meowify(text: String): String {
         if (text.isBlank()) return text
 
-        val words = "\\S+".toRegex().findAll(text).count()
-        return if (words == 0) text else List(words) { "meow" }.joinToString(" ")
+        return MEOWIFY_PATTERN.replace(text) { res ->
+            if (res.groupValues[1].isNotEmpty()) {
+                res.value
+            } else {
+                "meow"
+            }
+        }
     }
+
+    private val MEOWIFY_PATTERN = Regex("(${FORMATTING_CODE_PATTERN.pattern})|(\\p{L}+)", RegexOption.IGNORE_CASE)
+
 }
