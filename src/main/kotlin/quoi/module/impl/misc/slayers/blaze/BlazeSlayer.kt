2c2e04ec19295760872eb7510c43ea6d870a46a6
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-07-09 23:46:38 +0300

    feat(blazeslayer): gummy bear timer

--- src/main/kotlin/quoi/module/impl/misc/slayers/blaze/BlazeSlayer.kt
+++ src/main/kotlin/quoi/module/impl/misc/slayers/blaze/BlazeSlayer.kt
@@ -6,16 +6,22 @@ import net.minecraft.world.entity.monster.Blaze
 import net.minecraft.world.entity.monster.skeleton.WitherSkeleton
 import net.minecraft.world.entity.monster.zombie.ZombifiedPiglin
 import quoi.api.colour.Colour
+import quoi.api.events.ChatEvent
 import quoi.api.events.TickEvent
+import quoi.api.events.core.Event
+import quoi.api.events.core.on
 import quoi.api.events.core.trackedBy
 import quoi.api.skyblock.location.Island
+import quoi.api.skyblock.location.Location
 import quoi.module.impl.misc.slayers.ISlayer
 import quoi.module.impl.misc.slayers.QuestState
 import quoi.module.impl.misc.slayers.Slayers
 import quoi.module.settings.group.SettingGroup
 import quoi.utils.EntityUtils.getEntities
 import quoi.utils.EntityUtils.getEntity
+import quoi.utils.StringUtils.formatTime
 import quoi.utils.StringUtils.noControlCodes
+import quoi.utils.ui.textPair
 
 // todo auto aim.
 object BlazeSlayer : SettingGroup(Slayers, "Blaze", area = Island.CrimsonIsle, subarea = "smoldering tomb"), ISlayer {
@@ -24,6 +30,29 @@ object BlazeSlayer : SettingGroup(Slayers, "Blaze", area = Island.CrimsonIsle, s
         AutoAttune,
         DamageDodge
     )
+
+    private var gummyExpires by slider("gummy_expires", 0L, 0L, Long.MAX_VALUE).hide()
+    @Suppress("unused")
+    private val gummyBear by textHud("Gummy bear timer") {
+        visibleIf { Location.currentArea == Island.CrimsonIsle }
+        textPair(
+            string = "Gummy bear:",
+            supplier = {
+                val remaining = gummyExpires - System.currentTimeMillis()
+                if (remaining <= 0) "&cInactive" else formatTime(remaining, 0)
+            },
+            labelColour = colour,
+            shadow = shadow,
+            font = font
+        )
+    }.withSettings(::gummyExpires).setting(desc = "Re-heated gummy polar bear timer").asParent()
+
+    init {
+        on<ChatEvent.Packet> {
+            if (unformatted == "You ate a Re-heated Gummy Polar Bear!")
+                gummyExpires = System.currentTimeMillis() + 3_600_000L
+        }
+    }
 
     inline val blazeBoss: Blaze?
         get() = Slayers.currentBoss as? Blaze
@@ -75,6 +104,14 @@ object BlazeSlayer : SettingGroup(Slayers, "Blaze", area = Island.CrimsonIsle, s
 
         val name = stand?.customName?.string?.noControlCodes ?: return null
         return Attunement.entries.firstOrNull { name.contains(it.name, true) }
+    }
+
+    override fun shouldHandle(event: Event): Boolean {
+        if (event is ChatEvent.Packet) {
+            return inArea() //&& inSubarea()
+        }
+
+        return super.shouldHandle(event)
     }
 
     override val entitiesForRender: List<Pair<LivingEntity, Colour?>> // untested, maybe works
